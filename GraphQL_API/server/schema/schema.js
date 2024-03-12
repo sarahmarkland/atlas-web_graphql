40a34bd4b7361852ae41ffa500ac7d82cfe0d147
sarahmarkland <sarah.markland@icloud.com>
2024-03-12 16:34:11 -0500

    task 2, my greatest accomplishment.

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,7 +1,7 @@
 // required modules
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+const _ = require('lodash');
 
-// Define the TaskType GraphQLObjectType
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: {
@@ -12,6 +12,21 @@ const TaskType = new GraphQLObjectType({
   }
 });
 
+const tasks = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+  },
+];
+
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -19,24 +34,7 @@ const RootQueryType = new GraphQLObjectType({
       type: TaskType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        // resolve with data
-      }
-    }
-  }
-});
-
-const RootMutationType = new GraphQLObjectType({
-  name: 'RootMutationType',
-  fields: {
-    addTask: {
-      type: TaskType,
-      args: {
-        title: { type: GraphQLString },
-        weight: { type: GraphQLInt },
-        description: { type: GraphQLString }
-      },
-      resolve(parent, args) {
-        // resolve with data
+        return _.find(tasks, { id: args.id });
       }
     }
   }
@@ -44,5 +42,4 @@ const RootMutationType = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: RootQueryType,
-  mutation: RootMutationType
 });
