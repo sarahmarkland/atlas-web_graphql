0dcfb3e1e418e1f830678308a4294c4cdcbc5a99
sarahmarkland <sarah.markland@icloud.com>
2024-03-17 15:46:06 -0500

    task 4 working

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,39 +1,54 @@
 // required modules
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID, GraphQLList } = require('graphql');
 const _ = require('lodash');
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
-  fields: {
-    id: { type: GraphQLID },
-    title: { type: GraphQLString },
-    weight: { type: GraphQLInt },
-    description: { type: GraphQLString }
-  }
-});
+    fields: () => ({
+      id: { type: GraphQLID },
+      title: { type: GraphQLString },
+      weight: { type: GraphQLInt },
+      description: { type: GraphQLString },
+      projectId: { type: GraphQLID },
+      project: {
+        type: ProjectType,
+        resolve(parent, args) {
+          return _.find(projects, { id: parent.projectId });
+        }
+      }
+    })
+  });
 
 const ProjectType = new GraphQLObjectType({
   name: 'Project',
-  fields: {
-    id: { type: GraphQLID },
-    title: { type: GraphQLString },
-    weight: { type: GraphQLInt },
-    description: { type: GraphQLString }
-  }
-});
+    fields: () => ({
+      id: { type: GraphQLID },
+      title: { type: GraphQLString },
+      weight: { type: GraphQLInt },
+      description: { type: GraphQLString },
+      tasks: {
+        type: new GraphQLList(TaskType),
+        resolve(parent, args) {
+          return _.filter(tasks, { projectId: parent.id });
+        }
+      }
+    })
+  }); // Add a closing parenthesis here
 
 const tasks = [
   {
     id: '1',
     title: 'Create your first webpage',
     weight: 1,
-    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+    projectId: '1'
   },
   {
     id: '2',
     title: 'Structure your webpage',
     weight: 1,
-    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+    projectId: '2'
   },
 ];
 
