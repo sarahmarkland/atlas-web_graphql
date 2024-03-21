c7e13e4b0e063cf8aab7fa69d0f364235c9af39e
sarahmarkland <sarah.markland@icloud.com>
2024-03-20 21:07:46 -0500

    task 9

---
+++ GraphQL_API/client/src/components/TaskList.js
@@ -0,0 +1,19 @@
+// import React, { useState } from 'react';
+import TaskDetails from './TaskDetails';
+
+function TaskList(props) {
+  // const [state, setState] = useState({
+  //   selected: null
+  // });
+
+  return ( 
+    <div>
+      <ul id="task-list">
+        {}
+      </ul>
+      <TaskDetails/> 
+    </div>
+  );
+}
+
+export default TaskList;
