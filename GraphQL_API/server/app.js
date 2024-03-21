88f4ee8836086f4d148b055b77f3c663d9b9a712
sarahmarkland <sarah.markland@icloud.com>
2024-03-21 15:20:07 -0500

    task 10 v2

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -16,6 +16,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const cors = require('cors');
+
 
 mongoose.connect('mongodb+srv://sarahmarkland:zkVgyySx8Z9jF3Ql@cluster0.hhjpjnw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
@@ -23,6 +25,7 @@ mongoose.connect('mongodb+srv://sarahmarkland:zkVgyySx8Z9jF3Ql@cluster0.hhjpjnw.
 });
 
 const app = express();
+app.use(cors());
 
 mongoose.connection.once('open', () => {
   console.log('connected to database');
